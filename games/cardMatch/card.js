8e2b8dac9766777cf70b976c085f55385ec2d4a4
KarolisKaj <karolis.kajenas1@gmail.com>
2020-05-04 08:00:29 +0800

    Card game board in place, need to adjust flipping pair login

--- games/cardMatch/card.js
+++ games/cardMatch/card.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Animated } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, TouchableOpacity, Animated } from 'react-native';
+import CardFlip from 'react-native-card-flip';
 
 export default class Card extends Component {
     constructor(props) {
         super(props)
         this.state = {
             displayValue: props.displayValue,
-            isFlipped: false
+            isFlipped: false,
+            notifyFlipped: props.notifyFlipped,
+            locked: false
         }
     }
 
+    lockCard() {
+        this.setState({ locked: true })
+    }
+
     flipCard() {
+        if (this.state.locked) return
         this.setState({ isFlipped: !this.state.isFlipped });
+        this.card.flip()
+        this.state.notifyFlipped(this.state.displayValue, this.props.id, this.state.isFlipped)
     }
 
     render() {
         return (
-            <View>
-                <Text>111</Text>
-            </View>
+            <CardFlip style={styles.cardContainer} ref={(card) => this.card = card} >
+                <TouchableOpacity style={styles.openedCard} onPress={() => this.flipCard()} ><Text>Click me!</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.closedCard} onPress={() => this.flipCard()} ><Text>{this.state.displayValue}</Text></TouchableOpacity>
+            </CardFlip>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    openedCard: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: 'lightblue'
+    },
+    closedCard: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: 'yellow'
+    }
+})
--- games/cardMatch/cardMatchBoard.js
+++ games/cardMatch/cardMatchBoard.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Card from './card.js'
 
 
-function generateBoard(size) {
+function generateBoard(size, notifyFlipped) {
     let possibleValues = (size * size / 2)
     let board = []
     for (let i = 0; i < possibleValues; i++) {
         let cardRef1 = React.createRef()
-        board.splice(Math.floor(Math.random() * board.length), 0, { element: (<Card key={`1x${i}`} ref={cardRef1}></Card>), ref: cardRef1 });
+        board.splice(Math.floor(Math.random() * board.length), 0, { element: (<Card key={`1x${i}`} id={`1x${i}`} ref={cardRef1} displayValue={i} notifyFlipped={notifyFlipped}></Card>), ref: cardRef1 });
         let cardRef2 = React.createRef()
-        board.splice(Math.floor(Math.random() * board.length), 0, { element: (<Card key={`2x${i}`} ref={cardRef2}></Card>), ref: cardRef2 });
+        board.splice(Math.floor(Math.random() * board.length), 0, { element: (<Card key={`2x${i}`} id={`2x${i}`} ref={cardRef2} displayValue={i} notifyFlipped={notifyFlipped}></Card>), ref: cardRef2 });
     }
 
     let result = []
@@ -26,7 +26,16 @@ export default class CardMatchBoard extends Component {
         let boardSize = 4
         this.state = {
             boardSize: boardSize,
-            board: generateBoard(boardSize)
+            board: generateBoard(boardSize, this.handleCardInteraction),
+            cardsFlipped: []
+        }
+    }
+
+    handleCardInteraction(displayValue, id, isFlipped) {
+        if (isFlipped && cardsFlipped.length === 1) {
+            let cardFlippedExisting = this.state.cardsFlipped.find(x => x.ref.state.displayValue === displayValue)
+            let cardFlipped = this.state.board.find(x => x.ref.state.id === id)
+
         }
     }
 
