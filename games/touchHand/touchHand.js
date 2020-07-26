7658430a49bac39540b2d3d1608c52772dbfb5a8
KarolisKaj <karolis.kajenas1@gmail.com>
2020-07-26 21:08:42 +0800

    get animation into the app

--- games/touchHand/touchHand.js
+++ games/touchHand/touchHand.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import colorStyles from '../../styles/colorStyles'
+import LottieView from 'lottie-react-native';
 
 export default class TouchHand extends Component {
     constructor(props) {
@@ -9,14 +10,18 @@ export default class TouchHand extends Component {
         }
     }
 
-
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.textContainer}>
                     <Text>Touch Hand</Text>
                 </View>
-            </View>
+                <View style={styles.contentContainer}>
+                    {/* <LottieView source={require('../../styles/animations/whale/WhaleFinAnimation.json')} imageAssetsFolder={'lottie/whaleAnimation'} autoPlay={true} loop={true} autoSize={true} /> */}
+                    {/* <LottieView source={require('../../styles/animations/testAnimation/joyParty.json')} autoPlay={true} loop={true} autoSize={true} /> */}
+                    <LottieView style={{flex:1}} source={require('../../styles/animations/testAnimation/joyParty.json')} autoplay loop/>
+                </View>
+            </View >
         );
     }
 }
@@ -29,6 +34,14 @@ const styles = StyleSheet.create({
     },
     textContainer: {
         flex: 1,
+        alignItems: 'stretch'
+    },
+    contentContainer: {
+        minHeight:100,
+        minWidth:100,
+        height:200,
+        width:200,
+        flex: 9,
         alignItems: 'stretch'
     }
 })
