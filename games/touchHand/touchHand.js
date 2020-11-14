3285c2a9e3371f7c99ce8a5b7dec07096bfb3ba0
Karolis Kajenas <kkajenas@bloomberg.net>
2020-11-14 20:32:22 +0000

    lottie animations working finally

--- games/touchHand/touchHand.js
+++ games/touchHand/touchHand.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import colorStyles from '../../styles/colorStyles'
 import LottieView from 'lottie-react-native';
+import Draggable from 'react-native-draggable';
 
 export default class TouchHand extends Component {
     constructor(props) {
         super(props)
         this.state = {
         }
+    }
+
+    componentDidMount() {
+        this.endGlow()
+        this.endTouch()
+    }
+
+    startGlowing() {
+        this.glowAnimation.play();
+    }
+
+    endGlow() {
+        this.glowAnimation.play(34, 34);
+        this.glowAnimation.pause();
+    }
+    startTouch() {
+        this.touchAnimation.play();
+    }
+
+    endTouch() {
+        this.touchAnimation.play(300, 300);
+        this.touchAnimation.pause();
     }
 
     render() {
@@ -17,9 +40,10 @@ export default class TouchHand extends Component {
                     <Text>Touch Hand</Text>
                 </View>
                 <View style={styles.contentContainer}>
-                    {/* <LottieView source={require('../../styles/animations/whale/WhaleFinAnimation.json')} imageAssetsFolder={'lottie/whaleAnimation'} autoPlay={true} loop={true} autoSize={true} /> */}
-                    {/* <LottieView source={require('../../styles/animations/testAnimation/joyParty.json')} autoPlay={true} loop={true} autoSize={true} /> */}
-                    <LottieView style={{flex:1}} source={require('../../styles/animations/testAnimation/joyParty.json')} autoplay loop/>
+                    <LottieView style={{ flex: 1 }} source={require('../../styles/animations/touch/blue-glow.json')}
+                        ref={glowAnimation => { this.glowAnimation = glowAnimation; }} />
+                    <LottieView style={{ flex: 1 }} source={require('../../styles/animations/touch/finger-scan.json')}
+                        ref={touchAnimation => { this.touchAnimation = touchAnimation; }} />
                 </View>
             </View >
         );
@@ -37,10 +61,10 @@ const styles = StyleSheet.create({
         alignItems: 'stretch'
     },
     contentContainer: {
-        minHeight:100,
-        minWidth:100,
-        height:200,
-        width:200,
+        minHeight: 100,
+        minWidth: 100,
+        height: 200,
+        width: 200,
         flex: 9,
         alignItems: 'stretch'
     }
