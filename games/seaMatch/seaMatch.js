3f2e111654aa92e27a0bac7ecae13d28eb0e60fd
Karolis Kajenas <kkajenas@bloomberg.net>
2020-12-23 22:48:01 +0000

    fixed minor issues to sea match, included wining animation

--- games/seaMatch/seaMatch.js
+++ games/seaMatch/seaMatch.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import colorStyles from '../../styles/colorStyles'
 import Draggable from 'react-native-draggable';
-import { Square, Rectangle, Circle, Oval, Octagon, Trapezoid } from 'react-native-shape';
+import { Square, Rectangle, Circle, Oval, Octagon, Trapezoid, Triangle, Hexagon, Pentagon } from 'react-native-shape';
+import * as Animatable from 'react-native-animatable';
 
 export default class SeaMatch extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isTransition: false,
             timeLeft: 30,
             timer: undefined,
             shapeCount: 6,
@@ -16,18 +16,32 @@ export default class SeaMatch extends Component {
             margin: 30,
             oneSpaceHeight: Dimensions.get('window').height / 7,
             colors: ['yellow', 'purple', 'black', 'red', 'blue', 'orange', 'magenta'],
-            shapes: [(color) => <Square color={color} />, (color) => <Rectangle color={color} />, (color) => <Circle color={color} />, (color) => <Oval color={color} />, (color) => <Octagon color={color} />, (color) => <Trapezoid color={color} />],
+            shapes: [(color) => <Square color={color} />, (color) => <Rectangle color={color} />, (color) => <Circle color={color} />, (color) => <Hexagon color={color} />, (color) => <Octagon color={color} />, (color) => <Pentagon color={color} />],
             allShapes: undefined,
             shapesMap: undefined,
-
+            timeElapsed: 0,
+            gameFinished: false
         }
+    }
+
+    componentDidMount() {
+        this.createShapesMap()
+        this.setState({ allShapes: this.createShapes(this.state.shapesMap) });
+        this.state.timer = setInterval(() => {
+            if (!this.state.gameFinished)
+                this.setState({ timeElapsed: this.state.timeElapsed + 1 })
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.state.timer);
     }
 
     getRandomArray() {
         return Array.from({ length: this.state.shapeCount }, (_, k) => k).sort(() => .5 - Math.random());
     }
 
-    componentDidMount() {
+    createShapesMap() {
         let slots = this.getRandomArray();
         let shapes = this.getRandomArray();
         this.state.shapesMap = Array(this.state.shapeCount).fill().map((_, i) => {
@@ -39,13 +53,11 @@ export default class SeaMatch extends Component {
                 slotStartY: slotStartHeight,
                 slotStartX: margin,
                 shapeStartY: shapeStartHeight,
-                shapeStartX: shapeXPosition,
+                shapeStartX: shapeXPosition - this.state.margin * 2,
                 color: this.state.colors[shapes[i]],
                 isMatched: false
             }
         });
-        this.setState({ allShapes: this.createShapes(this.state.shapesMap) });
-        console.log(this.state.shapesMap);
     }
 
     createShapes(shapesMap) {
@@ -53,16 +65,18 @@ export default class SeaMatch extends Component {
             let getTopStyle = () => { return { top: shapeMap.slotStartY, position: 'absolute', left: this.state.margin } };
             return {
                 shape: (!shapeMap.isMatched && <Draggable
+                    key={`${shapeMap.shapeStartY}${shapeMap.shapeStartX}`}
                     x={shapeMap.shapeStartX}
                     y={shapeMap.shapeStartY}
                     renderSize={this.state.sizeOfShape}
                     renderColor={'transperant'}
                     shouldReverse
                     onDragRelease={((_, gestureState, __) => {
-                        console.log(shapeMap)
                         if (shapeMap.slotStartX < gestureState.moveX && gestureState.moveX < (shapeMap.slotStartX + this.state.sizeOfShape)
                             && shapeMap.slotStartY < gestureState.moveY && gestureState.moveY < (this.state.sizeOfShape + shapeMap.slotStartY)) {
                             shapeMap.isMatched = true;
+                            if (this.state.shapesMap.every(x => x.isMatched))
+                                this.finishGame();
                             this.setState({ allShapes: this.createShapes(this.state.shapesMap) });
                         }
                     }).bind(this)} >
@@ -70,28 +84,39 @@ export default class SeaMatch extends Component {
                         {this.state.shapes[i](shapeMap.color)}
                     </View>
                 </Draggable>),
-                slot: (<View style={getTopStyle()} >
+                slot: (<View style={getTopStyle()} key={`${shapeMap.slotStartY}${shapeMap.slotStartX}`}>
                     { this.state.shapes[i](shapeMap.isMatched ? shapeMap.color : 'grey')}
                 </ View>)
             }
         });
     }
 
-    componentWillUnmount() {
-
+    finishGame() {
+        this.state.gameFinished = true;
+        setTimeout(function () { this.props.navigation.navigate('GameOfLife') }.bind(this), 5000);
+        setTimeout(function () { this.props.navigation.navigate('Home') }.bind(this), 10000);
     }
 
     render() {
         return (
             <View style={styles.container}>
+
                 <View style={styles.textContainer}>
                     <Text>Sea Match</Text>
                 </View>
-                <View>
+                <View style={styles.scoreContainer}>
+                    <Text style={styles.bigText}>Time passed: {this.state.timeElapsed}</Text>
+                </View>
+                <View style={{ ...styles.contentContainer, ...{ backgroundColor: 'transperant' } }}>
                     {this.state.allShapes && this.state.allShapes.map(x => x.shape)}
                     {this.state.allShapes && this.state.allShapes.map(x => x.slot)}
                 </View>
-            </View>
+                {
+                    this.state.gameFinished && <View style={styles.center}>
+                        <Animatable.Text style={styles.bigText} animation="pulse" easing="ease-out" iterationCount="infinite">You won in {this.state.timeElapsed} seconds!</Animatable.Text>
+                    </View>
+                }
+            </View >
         );
     }
 }
@@ -100,17 +125,21 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: colorStyles.lowIntenseColor,
-        flexDirection: 'row-reverse',
+        flexDirection: 'column',
     },
     textContainer: {
         flex: 1,
         alignItems: 'center',
+        margin: 20
+    },
+    scoreContainer: {
+        flex: 19,
+        alignItems: 'center',
     },
     contentContainer: {
-        alignItems: 'center',
-        flex: 9,
-        // backgroundColor: 'yellow',
-        justifyContent: 'center'
+        position: "absolute",
+        top: 0, left: 0,
+        right: 0, bottom: 0,
     },
     center: {
         position: 'absolute',
