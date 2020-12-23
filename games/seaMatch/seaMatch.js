b1104dd4184f0991332cd4fe2286342c19e19f85
Karolis Kajenas <kkajenas@bloomberg.net>
2020-12-23 21:28:27 +0000

    match shape implement, only left to polish

--- games/seaMatch/seaMatch.js
+++ games/seaMatch/seaMatch.js
@@ -2,21 +2,79 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import colorStyles from '../../styles/colorStyles'
 import Draggable from 'react-native-draggable';
+import { Square, Rectangle, Circle, Oval, Octagon, Trapezoid } from 'react-native-shape';
 
 export default class SeaMatch extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            windowWidth: Dimensions.get('window').width,
-            windowWidth: Dimensions.get('window').height,
             isTransition: false,
             timeLeft: 30,
-            timer: undefined
+            timer: undefined,
+            shapeCount: 6,
+            sizeOfShape: 50,
+            margin: 30,
+            oneSpaceHeight: Dimensions.get('window').height / 7,
+            colors: ['yellow', 'purple', 'black', 'red', 'blue', 'orange', 'magenta'],
+            shapes: [(color) => <Square color={color} />, (color) => <Rectangle color={color} />, (color) => <Circle color={color} />, (color) => <Oval color={color} />, (color) => <Octagon color={color} />, (color) => <Trapezoid color={color} />],
+            allShapes: undefined,
+            shapesMap: undefined,
+
         }
     }
 
-    componentDidMount() {
+    getRandomArray() {
+        return Array.from({ length: this.state.shapeCount }, (_, k) => k).sort(() => .5 - Math.random());
+    }
 
+    componentDidMount() {
+        let slots = this.getRandomArray();
+        let shapes = this.getRandomArray();
+        this.state.shapesMap = Array(this.state.shapeCount).fill().map((_, i) => {
+            let slotStartHeight = (slots[i] + 1) * this.state.oneSpaceHeight;
+            let shapeStartHeight = (shapes[i] + 1) * this.state.oneSpaceHeight;
+            let margin = (this.state.oneSpaceHeight - this.state.sizeOfShape) / 2;
+            let shapeXPosition = Dimensions.get('window').width - this.state.sizeOfShape;
+            return {
+                slotStartY: slotStartHeight,
+                slotStartX: margin,
+                shapeStartY: shapeStartHeight,
+                shapeStartX: shapeXPosition,
+                color: this.state.colors[shapes[i]],
+                isMatched: false
+            }
+        });
+        this.setState({ allShapes: this.createShapes(this.state.shapesMap) });
+        console.log(this.state.shapesMap);
+    }
+
+    createShapes(shapesMap) {
+        return shapesMap.map((shapeMap, i) => {
+            let getTopStyle = () => { return { top: shapeMap.slotStartY, position: 'absolute', left: this.state.margin } };
+            return {
+                shape: (!shapeMap.isMatched && <Draggable
+                    x={shapeMap.shapeStartX}
+                    y={shapeMap.shapeStartY}
+                    renderSize={this.state.sizeOfShape}
+                    renderColor={'transperant'}
+                    shouldReverse
+                    onDragRelease={((_, gestureState, __) => {
+                        console.log(shapeMap)
+                        if (shapeMap.slotStartX < gestureState.moveX && gestureState.moveX < (shapeMap.slotStartX + this.state.sizeOfShape)
+                            && shapeMap.slotStartY < gestureState.moveY && gestureState.moveY < (this.state.sizeOfShape + shapeMap.slotStartY)) {
+                            shapeMap.isMatched = true;
+                            this.setState({ allShapes: this.createShapes(this.state.shapesMap) });
+                        }
+                    }).bind(this)} >
+                    <View style={{ width: this.state.sizeOfShape, width: this.state.sizeOfShape }}>
+                        {this.state.shapes[i](shapeMap.color)}
+                    </View>
+                </Draggable>),
+                slot: (<View style={getTopStyle()} >
+                    { this.state.shapes[i](shapeMap.isMatched ? shapeMap.color : 'grey')}
+                </ View>)
+            }
+        });
     }
 
     componentWillUnmount() {
@@ -25,12 +83,14 @@ export default class SeaMatch extends Component {
 
     render() {
         return (
-            <View>
-                <Draggable x={75} y={100} renderSize={56} renderColor='black' renderText='A' isCircle shouldReverse onShortPressRelease={(event) => console.log(event)} />
-                <Draggable x={200} y={300} renderColor='red' renderText='B' />
-                <Draggable />
-                <Draggable x={50} y={50}>
-                </Draggable>
+            <View style={styles.container}>
+                <View style={styles.textContainer}>
+                    <Text>Sea Match</Text>
+                </View>
+                <View>
+                    {this.state.allShapes && this.state.allShapes.map(x => x.shape)}
+                    {this.state.allShapes && this.state.allShapes.map(x => x.slot)}
+                </View>
             </View>
         );
     }
